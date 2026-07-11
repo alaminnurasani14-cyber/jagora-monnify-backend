b2f17c1271d47a50967bbfe200a3aa4bb8a5d4dc
alaminnurasani14-cyber <alaminnurasani14@gmail.com>
2026-07-11 17:21:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
+const express = require("express");
 require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "JAGORA ENTERPRISE Monnify Backend is running"
-  });
+  res.send("JAGORA Monnify Backend is running...");
 });
 
 const PORT = process.env.PORT || 3000;
